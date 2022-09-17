28f7f83293e5721ec6394f71b1d30e0a0735b9d1
Trevor <25110388+TrevTV@users.noreply.github.com>
2022-09-17 12:22:33 -0400

    Fix issue #5

--- CurseForge Instant Install.user.js
+++ CurseForge Instant Install.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         CurseForge Instant Download
-// @version      1.0.2
+// @version      1.0.3
 // @description  Instant downloading of Minecraft CurseForge mods.
 // @author       TrevTV
 // @match        https://www.curseforge.com/minecraft/mc-mods/*/download/*
@@ -8,12 +8,17 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-    var queryURL = document.querySelector('.text-sm a').href // Rewritten by origamitaco (Tyler Harrison)
+(async function() {
+    'use strict';
+    var queryURL = document.querySelector('.text-sm a').href; // Rewritten by origamitaco (Tyler Harrison)
     window.location.replace(queryURL);
     var countdown = document.getElementsByClassName("text-xl")[0];
     countdown.remove();
+    await sleep(2000);
+    window.history.go(-1);
 }
 )();
